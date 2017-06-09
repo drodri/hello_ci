7d0e0ef1ebe1d2f1700ce63fed6820c9fda6bd10
memsharded <diego.rlosada@gmail.com>
2017-06-09 17:39:57 +0200

    named pages

--- build.py
+++ build.py
@@ -4,5 +4,5 @@ from conan.packager import ConanMultiPackager
 if __name__ == "__main__":
     builder = ConanMultiPackager(username="memsharded", channel="stable", visual_versions=["10", "12", "14", "15"])
     builder.add_common_builds(shared_option_name="HelloCI:shared")
-    builder.use_default_named_builds()
+    builder.use_default_named_pages()
     builder.run()
