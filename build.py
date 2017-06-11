8bde648507ddf2d70aee79b683b816e79dcffebe
memsharded <diego.rlosada@gmail.com>
2017-06-12 00:35:19 +0200

    working on demo

--- build.py
+++ build.py
@@ -2,7 +2,7 @@ from conan.packager import ConanMultiPackager
 
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(username="memsharded", channel="stable", visual_versions=["10", "12", "14", "15"])
-    builder.add_common_builds(shared_option_name="HelloCI:shared")
+    builder = ConanMultiPackager(username="memsharded", channel="stable", visual_versions=["12", "14", "15"])
+    builder.add_common_builds(shared_option_name="HelloCi:shared")
     builder.use_default_named_pages()
     builder.run()
