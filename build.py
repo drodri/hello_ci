e4ac93f66b735269f4518d21b158f17ab9d96321
memsharded <diego.rlosada@gmail.com>
2017-06-12 13:38:33 +0200

    removed named builds

--- build.py
+++ build.py
@@ -4,5 +4,4 @@ from conan.packager import ConanMultiPackager
 if __name__ == "__main__":
     builder = ConanMultiPackager(username="drodri", channel="stable")
     builder.add_common_builds(shared_option_name="HelloCi:shared")
-    builder.use_default_named_pages()
     builder.run()
